705328cf09616d1250e7a545042d35d6584f7a75
geocostax <145722121+geocostax@users.noreply.github.com>
2024-05-10 22:05:26 -0300

    Add files via upload

---
+++ JS/filtro.js
@@ -0,0 +1,28 @@
+//Filtrando as tabelas
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function () {
+    var clientes = document.querySelectorAll(".cliente");
+
+    //Verifica se tem critério de busca
+    if (this.value.length > 0) {
+        for (var cli = 0; cli < clientes.length; cli++) {
+            var nome = clientes[cli].querySelector(".nome").textContent;
+
+            var expressao = new RegExp(this.value, "i");
+
+            //Verifica os clientes conforme critério de busca
+            if (!expressao.test(nome)) {
+                clientes[cli].classList.add("invisivel");
+            } else {
+                clientes[cli].classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var cli = 0; cli < clientes.length; cli++) {
+            var nome = clientes[cli].querySelector(".nome").textContent;
+
+            clientes[cli].classList.remove("invisivel");
+        }
+    }
+});
